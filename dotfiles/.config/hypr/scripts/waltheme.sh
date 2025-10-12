b31ce295753b1a5fd2c7e55579f66c3fa910ad46
shorinkiwata <fcl709@outlook.com>
2025-10-12 11:39:44 +0900

    q

---
+++ dotfiles/.config/hypr/scripts/waltheme.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+WALLPAPER_DIR="$HOME/Pictures/wallpaper"
+RANDOM_WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+
+matugen image "$RANDOM_WALLPAPER" && wal -n -q -i "$RANDOM_WALLPAPER"
---
+++ dotfiles/.vimrc
@@ -0,0 +1,8 @@
+"显示行号
+set number
+"语法高亮
+syntax on
+"显示相对行号
+set relativenumber
+"高亮当前行
+set cursorline
